744c610069ea484062035922369f402f6deae06c
hbemretasci <hbemretasci@gmail.com>
2023-02-06 22:02:39 +0300

    6.2.2023

--- middlewares/errors/customErrorHandler.js
+++ middlewares/errors/customErrorHandler.js
@@ -3,14 +3,17 @@ const CustomError = require('../../helpers/error/CustomError');
 const customErrorHandler = (err, req, res, next) => {
     let customError = err;
 
+    if(err.name === "MongooseError") {
+        customError = new CustomError("Database connection error.", 400);
+    }
     if(err.name === "SyntaxError") {
-        customError = new CustomError("Unexpected Syntax.", 400);
+        customError = new CustomError("Unexpected syntax.", 400);
     }
     if(err.name === "ValidationError") {
         customError = new CustomError("Validation error.", 400);
     }
     if(err.code === 11000) {
-        customError = new CustomError("Duplicate key found, please check your input.", 400);
+        customError = new CustomError("This email is already registered, please check your input.", 400);
     }
     if(err.name === "CastError") {
         customError = new CustomError("Please provide a valid id.", 400);
